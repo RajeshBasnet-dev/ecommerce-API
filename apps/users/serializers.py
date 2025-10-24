ed15808420d0118d703cba6ec0a42b8221755e84
Rajesh Basnet <backendrex@gmail.com>
2025-10-24 11:23:46 +0545

    feat: add user serializers for registration and authentication

---
+++ apps/users/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'role', 'first_name', 'last_name', 'phone_number', 'date_of_birth')
+        read_only_fields = ('id',)
+    
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        token['role'] = user.role
+        return token
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'role', 'first_name', 'last_name', 'phone_number', 'date_of_birth')
+        read_only_fields = ('id', 'role')
